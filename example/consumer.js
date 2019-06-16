ce691f93718360e8a347c3bc0fa4b0809009803a
myoungsubsim <rop2001@gmail.com>
2019-06-16 17:49:02 +0900

    add config.json

--- example/consumer.js
+++ example/consumer.js
@@ -2,8 +2,9 @@ import { Consumer } from '../src/consumer/Consumer';
 import { Channel } from '../src/Channel';
 import { Connection } from '../src/Connection';
 import { AMQP } from '../src/Protocol';
+import config from './config.json';
 
-const amqp = new AMQP('node:node@192.168.122.129');
+const amqp = new AMQP(`${config.user}:${config.password}@${config.host}:${config.port}`);
 const connection = new Connection(amqp);
 const channel = new Channel(connection, 'q');
 const consumer = new Consumer(channel);
